f9de260836ec52eb657e4c38f384accda0ec2fc6
dongquoctien(tom) <tien.dq@ohmyhotel.com>
2024-03-26 16:27:04 +0700

    init code

---
+++ Example/Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
