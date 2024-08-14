f3466efbc66ea16c5ba1e525e011ecf4dab3a31f
Tom <itdongquoctien@gmail.com>
2024-08-14 23:33:23 +0700

    init swagger

--- Example/Example.Web/Startup.cs
+++ Example/Example.Web/Startup.cs
@@ -26,6 +26,7 @@ namespace Example.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,10 @@ namespace Example.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+
             }
 
             app.UseHttpsRedirection();
@@ -46,6 +51,8 @@ namespace Example.WebAPI
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
